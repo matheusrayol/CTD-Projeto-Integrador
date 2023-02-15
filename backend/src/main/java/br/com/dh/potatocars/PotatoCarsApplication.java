a2cd7f4e48d864098787b8554ea0e75fcd30e455
tatianegardin <contatotatianegardin@hotmail.com>
2023-02-15 19:47:03 -0300

    feat: initializing backend

---
+++ backend/src/main/java/br/com/dh/potatocars/PotatoCarsApplication.java
@@ -0,0 +1,13 @@
+package br.com.dh.potatocars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotatoCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotatoCarsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/dh/potatocars/PotatoCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dh.potatocars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotatoCarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
