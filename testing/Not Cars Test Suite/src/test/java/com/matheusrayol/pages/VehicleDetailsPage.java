a3d0a257b991507895fb4ab3d3bb91cef34e80eb
Matheus Ferreira <matheusrbferreira@gmail.com>
2023-03-18 15:15:08 -0300

    test: updated testing framework

---
+++ testing/Not Cars Test Suite/src/test/java/com/matheusrayol/pages/VehicleDetailsPage.java
@@ -0,0 +1,4 @@
+package com.matheusrayol.pages;
+
+public class VehicleDetailsPage {
+}
---
+++ testing/Not Cars Test Suite/src/test/java/com/matheusrayol/tests/HomePageTests.java
@@ -0,0 +1,15 @@
+package com.matheusrayol.suite;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import java.util.List;
+
+// page_url = about:blank
+public class HomePageTests {
+
+    public HomePageTests(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+}
