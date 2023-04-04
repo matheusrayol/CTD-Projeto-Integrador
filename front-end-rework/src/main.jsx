5ebaacae5f5662c7b4f5df60a304cc56725783b5
Matheus Ferreira <matheusrbferreira@gmail.com>
2023-04-03 21:18:54 -0300

    feat: added first stage of frontend rework

---
+++ front-end-rework/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './styles/common.scss'
+
+import axios from 'axios'
+
+axios.defaults.baseURL = `http://localhost:8080`
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
---
+++ front-end-rework/src/pages/Register.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import RegisterValidation from '../components/RegisterValidation'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+export default function Register() {
+    return (
+        <>
+            <Navbar />
+            <RegisterValidation />
+            <Footer />
+        </>
+    )
+}
