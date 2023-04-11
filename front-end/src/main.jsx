160e244809821e0596920b58cb76f4ea9518305d
Matheus Ferreira <matheusrbferreira@gmail.com>
2023-04-10 21:23:11 -0300

    fix: ajuste na url da API TravelGreen

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -7,7 +7,7 @@ import LogRocket from 'logrocket';
 
 LogRocket.init('sthqmi/travel-green');
 
-axios.defaults.baseURL = `http://localhost:8080`
+axios.defaults.baseURL = `http://ec2-3-22-250-39.us-east-2.compute.amazonaws.com:8080`
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
