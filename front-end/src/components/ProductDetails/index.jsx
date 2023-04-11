1388230ca6c00b01d5d469b791391be1405fe6d1
Matheus Ferreira <matheusrbferreira@gmail.com>
2023-04-11 08:11:07 -0300

    hotfix: removed lightbox since it breaks production environment

--- front-end/src/components/ProductDetails/index.jsx
+++ front-end/src/components/ProductDetails/index.jsx
@@ -1,17 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { RotatingLines } from 'react-loader-spinner'
-// import { DoubleCalendar } from '../CalendarTG/Double'
-// import { SingleCalendar } from '../CalendarTG/Single'
+import { DoubleCalendar } from '../CalendarTG/Double'
+import { SingleCalendar } from '../CalendarTG/Single'
 import TGLeaf from '../../assets/travelgreen_leaf.svg'
 import { useAuth } from '../../hooks/useAuth'
-import FsLightbox from 'fslightbox-react'
 
 import axios from 'axios'
 
 export default function ProductDetails() {
-
-    const [toggler, setToggler] = useState(false)
 
     const location = useLocation().pathname.split('/')[2]
     const [product, setProduct] = useState()
@@ -118,6 +115,7 @@ export default function ProductDetails() {
                         <div className="container text-white">
                             <div className="row d-flex justify-content-center align-items-center">
                                 <div className="col">
+                        
                                     <span className="text-truncate d-flex align-items-center ubuntu fs-5 ms-3">
                                         <svg className="bi bi-pin-map-fill me-2 fs-7" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
                                             <path fillRule="evenodd" d="M3.1 11.2a.5.5 0 0 1 .4-.2H6a.5.5 0 0 1 0 1H3.75L1.5 15h13l-2.25-3H10a.5.5 0 0 1 0-1h2.5a.5.5 0 0 1 .4.2l3 4a.5.5 0 0 1-.4.8H.5a.5.5 0 0 1-.4-.8l3-4z"></path>
@@ -167,34 +165,9 @@ export default function ProductDetails() {
                                 <div className="col-lg-12 col-xl-7 col-xxl-7 d-none d-lg-flex">
                                     <div>
                                         <div className="img-container">
-                                            <img id="expandedImg" className="rounded w-100" src={expandedImgSrc ? expandedImgSrc : product.images[0].urlImage} alt={imgText} onClick={() => setToggler(!toggler)} />
+                                            <img id="expandedImg" className="rounded w-100" src={expandedImgSrc ? expandedImgSrc : product.images[0].urlImage} alt={imgText} />
                                             <div id="className"></div>
                                         </div>
-                                        <FsLightbox
-                                            key={product.id + '-lightbox'}
-                                            toggler={toggler}
-                                            sources={[
-                                                expandedImgSrc ? expandedImgSrc : product.images[0].urlImage,
-                                                product.images[1].urlImage,
-                                                product.images[2].urlImage,
-                                                product.images[3].urlImage,
-                                                product.images[4].urlImage
-                                            ]}
-                                            thumbs={[
-                                                null,
-                                                null,
-                                                null,
-                                                null,
-                                                null
-                                            ]}
-                                            types={[
-                                                'image',
-                                                'image',
-                                                'image',
-                                                'image',
-                                                'image'
-                                            ]}
-                                        />
                                         <div className="row gx-2">
                                             <div className="col column">
                                                 <img className="img-thumbnail img-square shadow" src={product.images[0].urlImage} alt={product.name} onClick={handleImgClick} />
@@ -319,7 +292,6 @@ export default function ProductDetails() {
                                                                     <div className="d-flex mb-2">
                                                                         <div className="bs-icon-sm bs-icon-circle text-bg-success d-flex flex-shrink-0 justify-content-center align-items-center d-inline-block bs-icon me-2">
                                                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -64 640 640" width="1em" height="1em" fill="currentColor">
-                                                                                {/* <!--! Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. --> */}
                                                                                 <path d="M96 96C96 60.65 124.7 32 160 32H576C611.3 32 640 60.65 640 96V320C640 355.3 611.3 384 576 384H160C124.7 384 96 355.3 96 320V96zM160 320H224C224 284.7 195.3 256 160 256V320zM160 96V160C195.3 160 224 131.3 224 96H160zM576 256C540.7 256 512 284.7 512 320H576V256zM512 96C512 131.3 540.7 160 576 160V96H512zM368 128C323.8 128 288 163.8 288 208C288 252.2 323.8 288 368 288C412.2 288 448 252.2 448 208C448 163.8 412.2 128 368 128zM48 360C48 399.8 80.24 432 120 432H520C533.3 432 544 442.7 544 456C544 469.3 533.3 480 520 480H120C53.73 480 0 426.3 0 360V120C0 106.7 10.75 96 24 96C37.25 96 48 106.7 48 120V360z"></path>
                                                                             </svg>
                                                                         </div>
@@ -341,7 +313,6 @@ export default function ProductDetails() {
                                                                     <div className="d-flex mb-2">
                                                                         <div className="bs-icon-sm bs-icon-circle text-bg-success d-flex flex-shrink-0 justify-content-center align-items-center d-inline-block bs-icon me-2">
                                                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -64 640 640" width="1em" height="1em" fill="currentColor">
-                                                                                {/* <!--! Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. --> */}
                                                                                 <path d="M96 96C96 60.65 124.7 32 160 32H576C611.3 32 640 60.65 640 96V320C640 355.3 611.3 384 576 384H160C124.7 384 96 355.3 96 320V96zM160 320H224C224 284.7 195.3 256 160 256V320zM160 96V160C195.3 160 224 131.3 224 96H160zM576 256C540.7 256 512 284.7 512 320H576V256zM512 96C512 131.3 540.7 160 576 160V96H512zM368 128C323.8 128 288 163.8 288 208C288 252.2 323.8 288 368 288C412.2 288 448 252.2 448 208C448 163.8 412.2 128 368 128zM48 360C48 399.8 80.24 432 120 432H520C533.3 432 544 442.7 544 456C544 469.3 533.3 480 520 480H120C53.73 480 0 426.3 0 360V120C0 106.7 10.75 96 24 96C37.25 96 48 106.7 48 120V360z"></path>
                                                                             </svg>
                                                                         </div>
@@ -364,7 +335,6 @@ export default function ProductDetails() {
                                                                     <div className="d-flex mb-2">
                                                                         <div className="bs-icon-sm bs-icon-circle text-bg-success d-flex flex-shrink-0 justify-content-center align-items-center d-inline-block bs-icon me-2">
                                                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="-64 0 512 512" width="1em" height="1em" fill="currentColor">
-                                                                                {/* <!--! Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. --> */}
                                                                                 <path d="M256 0v128h128L256 0zM224 128L224 0H48C21.49 0 0 21.49 0 48v416C0 490.5 21.49 512 48 512h288c26.51 0 48-21.49 48-48V160h-127.1C238.3 160 224 145.7 224 128zM64 72C64 67.63 67.63 64 72 64h80C156.4 64 160 67.63 160 72v16C160 92.38 156.4 96 152 96h-80C67.63 96 64 92.38 64 88V72zM64 136C64 131.6 67.63 128 72 128h80C156.4 128 160 131.6 160 136v16C160 156.4 156.4 160 152 160h-80C67.63 160 64 156.4 64 152V136zM304 384c8.875 0 16 7.125 16 16S312.9 416 304 416h-47.25c-16.38 0-31.25-9.125-38.63-23.88c-2.875-5.875-8-6.5-10.12-6.5s-7.25 .625-10 6.125l-7.75 15.38C187.6 412.6 181.1 416 176 416H174.9c-6.5-.5-12-4.75-14-11L144 354.6L133.4 386.5C127.5 404.1 111 416 92.38 416H80C71.13 416 64 408.9 64 400S71.13 384 80 384h12.38c4.875 0 9.125-3.125 10.62-7.625l18.25-54.63C124.5 311.9 133.6 305.3 144 305.3s19.5 6.625 22.75 16.5l13.88 41.63c19.75-16.25 54.13-9.75 66 14.12c2 4 6 6.5 10.12 6.5H304z"></path>
                                                                             </svg>
                                                                         </div>
@@ -432,21 +402,21 @@ export default function ProductDetails() {
                                 <div className="col offset-xxl-0 d-flex flex-column py-4 px-4 rounded bg-white">
                                     <h4 className="poppins travelgreen-logo mb-4">Datas disponíveis</h4>
                                     <div className="text-center d-flex d-xl-none justify-content-center align-items-center align-content-center flex-grow-1">
-                                        {/* <SingleCalendar
+                                        <SingleCalendar
                                             id={dateRange}
                                             onSelectedRange={selectedRange}
                                             selectRange={selectRange}
                                             tileDisabled={tileDisabled}
-                                        /> */}
+                                        />
                                     </div>
                                     <div className="text-center d-none d-xl-flex justify-content-center align-items-center align-content-center flex-grow-1">
-                                        {/* <DoubleCalendar
+                                        <DoubleCalendar
                                             id={dateRange}
                                             onSelectedData={selectedRange}
                                             selectRange={selectRange}
                                             tileDisabled={tileDisabled}
                                             width="100%"
-                                        /> */}
+                                        />
                                     </div>
                                     <div className="text-center mt-3">
                                         {auth ? (
