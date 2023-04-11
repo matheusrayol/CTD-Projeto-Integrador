84687a7d6dc1f18ab2098ad64be0698b2a0476f3
Matheus Ferreira <matheusrbferreira@gmail.com>
2023-04-10 21:43:38 -0300

    fix: remoção do logRocket para production

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/common.scss'
 import axios from 'axios'
-import LogRocket from 'logrocket';
+// import LogRocket from 'logrocket';
 
-LogRocket.init('sthqmi/travel-green');
+// LogRocket.init('sthqmi/travel-green');
 
 axios.defaults.baseURL = `http://ec2-3-22-250-39.us-east-2.compute.amazonaws.com:8080`
 
